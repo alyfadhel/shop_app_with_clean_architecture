c56578cff5ad3f374a151bac5ac3e71212dcf3d4
alyfa <alyfadel67@gmail.com>
2022-11-11 03:22:46 +0200

    Initialize project get products

--- lib/features/home/presentation/widgets/builder_widget.dart
+++ lib/features/home/presentation/widgets/builder_widget.dart
@@ -6,6 +6,7 @@ import 'package:shop_app_with_clean_architecture/features/home/data/models/home_
 import 'package:shop_app_with_clean_architecture/features/home/domain/entities/home.dart';
 import 'package:shop_app_with_clean_architecture/features/home/presentation/controller/cubit/cubit.dart';
 import 'package:shop_app_with_clean_architecture/features/home/presentation/controller/cubit/states.dart';
+import 'package:shop_app_with_clean_architecture/features/home/presentation/widgets/build_products.dart';
 
 class BuilderWidget extends StatelessWidget {
   const BuilderWidget({Key? key}) : super(key: key);
@@ -59,113 +60,5 @@ class BuilderWidget extends StatelessWidget {
   }
 }
 
-class BuildProducts extends StatelessWidget {
-  final Products model;
-  const BuildProducts({Key? key,required this.model}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<HomeCubit, HomeStates>(
-      listener: (context, state) {
-
-      },
-      builder: (context, state) {
-        return Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: AppSize.s10,
-              ),
-              child: Stack(
-                alignment: AlignmentDirectional.bottomStart,
-                children: [
-                  Image(
-                    width: double.infinity,
-                    height: 200.0,
-                    image: NetworkImage(
-                      model.image,
-                    ),
-                  ),
-                  if (model.discount != 0)
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10.0,
-                      ),
-                      decoration: const BoxDecoration(
-                        color: Colors.red,
-                      ),
-                      child: const Text(
-                        'DISCOUNT',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 12.0,
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: [
-                  Text(
-                    model.name,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      height: 1.3,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        '${model.price.round()}',
-                        style: const TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10.0,
-                      ),
-                      if (model.discount != 0)
-                        Text(
-                          '${model.oldPrice.round()}',
-                          style: const TextStyle(
-                            color: Colors.red,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.lineThrough,
-                          ),
-                        ),
-                      const Spacer(),
-                      IconButton(
-                        onPressed: ()
-                        {
-                          //ShopCubit.get(context).changeFavorites(model.id!);
-                        },
-                        icon: const CircleAvatar(
-                          radius: 15.0,
-                          //backgroundColor: ShopCubit.get(context).favorites[model.id] == true ? Colors.blue :  Colors.grey,
-                          child: Icon(
-                            Icons.favorite_border,
-                            size: 14.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
 
