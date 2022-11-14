b96eb2d88aa92617a325262fc5800cc15ff19104
alyfa <alyfadel67@gmail.com>
2022-11-14 20:30:06 +0200

    get products

--- lib/features/home/presentation/screens/products_screen.dart
+++ lib/features/home/presentation/screens/products_screen.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_conditional_rendering/flutter_conditional_rendering.dart';
+import 'package:shop_app_with_clean_architecture/core/resources/values_manager.dart';
 import 'package:shop_app_with_clean_architecture/features/home/domain/entities/home.dart';
 import 'package:shop_app_with_clean_architecture/features/home/presentation/controller/cubit/cubit.dart';
 import 'package:shop_app_with_clean_architecture/features/home/presentation/controller/cubit/state.dart';
@@ -41,6 +42,7 @@ class BuilderItem extends StatelessWidget {
 
       },
       builder: (context, state) {
+        var cubit = HomeCubit.get(context);
         return SingleChildScrollView(
           child: Column(
             children: [
@@ -68,6 +70,19 @@ class BuilderItem extends StatelessWidget {
                     scrollDirection: Axis.horizontal,
                   )
               ),
+              const SizedBox(
+                height: AppSize.s30,
+              ),
+              GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                mainAxisSpacing: 1.0,
+                crossAxisSpacing: 1.0,
+                childAspectRatio: 1/1.8,
+                children: List.generate(home.data.products.length, (index) => BuildProducts(products: home.data.products[index]),
+              ),
+              ),
             ],
           ),
         );
@@ -75,4 +90,112 @@ class BuilderItem extends StatelessWidget {
     );
   }
 }
+
+class BuildProducts extends StatelessWidget {
+  final Products products;
+  const BuildProducts({Key? key,required this.products}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<HomeCubit,HomeStates>(
+      listener: (context, state) {
+
+      },
+      builder: (context, state) {
+        var cubit = HomeCubit.get(context);
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.bottomStart,
+              children: [
+                Image(
+                  width: double.infinity,
+                  height: 200.0,
+                  image: NetworkImage(
+                    products.image,
+                  ),
+                ),
+                if (products.discount != 0)
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                    ),
+                    child: const Text(
+                      'DISCOUNT',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Text(
+                    products.name,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      height: 1.3,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '${products.price.round()}',
+                        style: const TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10.0,
+                      ),
+                      if (products.discount != 0)
+                        Text(
+                          '${products.oldPrice.round()}',
+                          style: const TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.lineThrough,
+                          ),
+                        ),
+                      const Spacer(),
+                      IconButton(
+                        onPressed: ()
+                        {
+                          // ShopCubit.get(context).changeFavorites(model.id!);
+                        },
+                        icon:  const CircleAvatar(
+                          radius: 15.0,
+                          // backgroundColor: ShopCubit.get(context).favorites[model.id] == true ? Colors.blue :  Colors.grey,
+                          child: Icon(
+                            Icons.favorite_border,
+                            size: 14.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+  }
+
 
