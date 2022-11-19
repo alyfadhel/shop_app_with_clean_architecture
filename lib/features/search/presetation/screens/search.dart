c05d9a2d9421136d0af2ea6286ec860cacbf8995
alyfa <alyfadel67@gmail.com>
2022-11-19 14:33:29 +0200

    get search layout

--- lib/features/search/presetation/screens/search.dart
+++ lib/features/search/presetation/screens/search.dart
@@ -8,6 +8,7 @@ import 'package:shop_app_with_clean_architecture/features/home/presentation/widg
 import 'package:shop_app_with_clean_architecture/features/search/domain/entities/search.dart';
 import 'package:shop_app_with_clean_architecture/features/search/presetation/controller/cubit.dart';
 import 'package:shop_app_with_clean_architecture/features/search/presetation/controller/states.dart';
+import 'package:shop_app_with_clean_architecture/features/search/presetation/widgets/search_list.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -68,107 +69,4 @@ class SearchScreen extends StatelessWidget {
   }
 }
 
-class SearchList extends StatelessWidget {
-  final SearchData model;
-  final bool isDiscount;
-  const SearchList({Key? key,required this.model, this.isDiscount = false}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: AppSize.s10,
-          ),
-          child: Stack(
-            alignment: AlignmentDirectional.bottomStart,
-            children: [
-              Image(
-                width: double.infinity,
-                height: AppSize.s200,
-                image: NetworkImage(
-                  model.image,
-                ),
-              ),
-              if (model.discount != 0 && isDiscount)
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10.0,
-                  ),
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                  ),
-                  child: const Text(
-                    'DISCOUNT',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12.0,
-                    ),
-                  ),
-                ),
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: 8.0,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Text(
-                model.name,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  height: 1.3,
-                ),
-              ),
-              Row(
-                children: [
-                  Text(
-                    '${model.price}',
-                    style: const TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10.0,
-                  ),
-                  if (model.discount != 0 && isDiscount)
-                    Text(
-                      '${model.oldPrice}',
-                      style: const TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.lineThrough,
-                      ),
-                    ),
-                  const Spacer(),
-                  IconButton(
-                    onPressed: ()
-                    {
-                      // ShopCubit.get(context).changeFavorites(model.id!);
-                    },
-                    icon:  const CircleAvatar(
-                      radius: 15.0,
-                      // backgroundColor: ShopCubit.get(context).favorites[model.id] == true ? Colors.blue :  Colors.grey,
-                      child: Icon(
-                        Icons.favorite_border,
-                        size: 14.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
